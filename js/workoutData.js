4369fb896abac9dbe6126fb898fb58481694a678
DelphiData <ljbarg0@gmail.com>
2025-02-01 17:14:50 -0500

    Create workoutData.js

---
+++ js/workoutData.js
@@ -0,0 +1,94 @@
+
+export const workoutData = {
+    "programs": {
+        "muscle-builder": {
+            "name": "Muscle Builder",
+            "type": "intermediate",
+            "blocks": 2,
+            "weeksPerBlock": 4,
+            "daysPerWeek": 3,
+            "startDate": "2025-02-03",
+            "description": "A structured muscle-building program using suspension training, focusing on strength and hypertrophy.",
+            "workouts": {
+                "1-1-1": {
+                    "block": 1,
+                    "week": 1,
+                    "day": 1,
+                    "focus": "Chest and Back",
+                    "exercises": [
+                        {
+                            "name": "TRX Chest Press",
+                            "sets": 2,
+                            "reps": "6-8",
+                            "RPE": 7,
+                            "tempo": "4012",
+                            "rest": "0min",
+                            "notes": "Tuck elbows at 45\u00b0, keep shoulder blades contracted together, focus on squeezing your chest muscles together to push-up."
+                        },
+                        {
+                            "name": "TRX Y's",
+                            "sets": 2,
+                            "reps": "4-8",
+                            "RPE": 7,
+                            "tempo": "4012",
+                            "rest": "2min",
+                            "notes": "Focus on squeezing your shoulder blades and rear shoulders together, push elbows up and out."
+                        }
+                    ]
+                },
+                "1-1-2": {
+                    "block": 1,
+                    "week": 1,
+                    "day": 2,
+                    "focus": "Quads, Glutes, Hamstrings, Abs & Calves",
+                    "exercises": [
+                        {
+                            "name": "TRX 1 Leg Pistol Squat",
+                            "sets": 2,
+                            "reps": "4-6",
+                            "RPE": 7,
+                            "tempo": "4012",
+                            "rest": "0min",
+                            "notes": "Squeeze your hamstrings and glutes to descend down, squeeze your quads to push-up."
+                        },
+                        {
+                            "name": "TRX 1 Leg Reverse Lunge",
+                            "sets": 2,
+                            "reps": "8-12",
+                            "RPE": 7,
+                            "tempo": "4012",
+                            "rest": "0min",
+                            "notes": "Minimize the amount you push off your rear leg, squeeze your quads to push-up."
+                        }
+                    ]
+                },
+                "1-1-3": {
+                    "block": 1,
+                    "week": 1,
+                    "day": 3,
+                    "focus": "Shoulders, Biceps, and Triceps",
+                    "exercises": [
+                        {
+                            "name": "TRX T's",
+                            "sets": 2,
+                            "reps": "4-6",
+                            "RPE": 7,
+                            "tempo": "4012",
+                            "rest": "0min",
+                            "notes": "Focus on squeezing your shoulder blades together, push elbows out and down."
+                        },
+                        {
+                            "name": "TRX Front Raise",
+                            "sets": 2,
+                            "reps": "6-8",
+                            "RPE": 7,
+                            "tempo": "4012",
+                            "rest": "0min",
+                            "notes": "Focus on contracting your shoulder muscles to move body, shoulder blades retracted."
+                        }
+                    ]
+                }
+            }
+        }
+    }
+}
